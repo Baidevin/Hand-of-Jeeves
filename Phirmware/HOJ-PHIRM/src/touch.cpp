cb547c2b84fae9649333b5de44bb32aa96007edc
lnederhof <lnederhof01@gmail.com>
2025-08-09 21:49:51 +1000

    deezv2electricboogaloo

--- Phirmware/HOJ-PHIRM/src/touch.cpp
+++ Phirmware/HOJ-PHIRM/src/touch.cpp
@@ -2,30 +2,33 @@
 
 #include <Arduino.h>
 
-
-TouchSensor::TouchSensor(int pin)
-{
-    _pin = pin;
+TouchSensor::TouchSensor(int pin){
     pinMode(pin, INPUT);
-    _itr = 0;
+    _sensorReference = touchRead(pin);
 }
 
-bool TouchSensor::isTouched()
-{
-    int avg = 0;
-    for (int i=0; i < BUFFER_LENGTH - 1; i++)
-    {
-        avg += _values[i];
+int TouchSensor::getSensorState(){
+
+    double average = 0;
+    sensorState state;
+    for(int i = 0; i < BUFFER_LENGTH; i++){
+        average += _values[i];
     }
-    avg = avg / BUFFER_LENGTH;
+    average = average / BUFFER_LENGTH;
 
-    return avg < ON_THRESH;
+    if (average <= Lower){
+        state = TOUCHTHUMB;
+    } else if (average >= Upper){
+        state = TOUCHUPPERPALM;
+    } else {
+        state = TOUCHLOWERPALM;
+    }
+    
+    return state;
 }
 
-void TouchSensor::update()
-{
+void TouchSensor::update() {
     _itr++;
     _itr = _itr % 5;
     _values[_itr] = touchRead(_pin);
-
 }
